c7e279d4add776a92015a30e54d82fbd4f4dcdb8
zhangrui <1521736922@qq.com [credential] helper = store>
2017-03-30 00:09:25 +0800

    zhangrui

--- SimpleFactory/SimpleFactory/Properties/AssemblyInfo.cs
+++ SimpleFactory/SimpleFactory/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("0fa23263-daa7-47f4-ae1f-ef00fb15b3f8")]
+[assembly: Guid("e02996ac-d56c-4b96-97ac-8d4a1fe601e3")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
